1326c7b3e7e194ba5ad45f2b4088e9c4d4c12531
kev-pinto-qodea <kev.pinto@qodea.com>
2025-07-17 11:47:06 +0000

    Addition of Makefile and Make prompt more explicit

--- sql/sample_fact.sql
+++ sql/sample_fact.sql
@@ -23,8 +23,9 @@ DECLARE v_defnulldttm   DEFAULT orgx_cntl.default_vars().defnulldttm;
 
 
 DECLARE v_mindate TIMESTAMP;
+DECLARE CurrentDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP();
 DECLARE v_mindate_int INT64;
-DECLARE v_MergeDateFilter INT64;
+DECLARE vMergeDateFilter INT64;
 DECLARE v_dim_source_record_set_key INT64 DEFAULT v_defnullnum;
 DECLARE v_max_sk INT64;
 
